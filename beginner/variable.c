2ebc74e251f8750098e103b1e74b86bef84a2448
karthick71198 <34646624+karthick71198@users.noreply.github.com>
2017-12-19 13:20:19 +0530

    Create variable.c

---
+++ beginner/variable.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{
+	char n[29]="Sachin Ramesh Tendulkar",nn[20]="Little Master";
+	int run=5473,match=786;
+	float avg=44.56;
+	printf("%s\n%s\n%d\n%.2f\n%d",n,nn,run,avg,match);
+}
+

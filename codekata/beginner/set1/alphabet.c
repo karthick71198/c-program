42c055921268b58c9be1f8c45b19480c6cec34c2
karthick71198 <34646624+karthick71198@users.noreply.github.com>
2017-12-20 20:28:38 +0530

    Create alphabet.c

---
+++ codekata/beginner/set1/alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    char c;
+    scanf("%c",&c);
+    if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
+        printf("alphabet",c);
+    else
+        printf("not alphabet",c);
+    return 0;
+}

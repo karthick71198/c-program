71649331d433c4b52f25ae762f2f84e53a21c6a5
karthick71198 <34646624+karthick71198@users.noreply.github.com>
2017-12-20 19:44:38 +0530

    Update Oddreven.c

--- codekata/beginner/set1/Oddreven.c
+++ codekata/beginner/set1/Oddreven.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
-int main(void) {
+int main() 
+{
 int Num;
-sacnf("℅d",&Num);
-if(Num℅2==0);
+scanf("℅d",&Num);
+if(Num % 2 == 0)
 printf("even",Num);
 else
 printf("Odd",Num);
-
-	return 0;
+return(0);
 }

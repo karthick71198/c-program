2862cef4719d08a3cb737c09ea543abbd7ed76f7
karthick71198 <34646624+karthick71198@users.noreply.github.com>
2017-12-21 15:01:55 +0530

    Update numofch.c

--- assignments/numofch.c
+++ assignments/numofch.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main(void) {
+	char s[50];
+	int i,j,count,l;
+	scanf("%s",&s);
+	l=strlen(s);
+	for(i=0;i<l;i++)
+	{
+		for(j=i+1;j<l;j++)
+		{
+		if(s[i]==s[j])
+		{
+		count++;
+		}
+		for(k=j)
+		
+		
+	}
+	
+	return 0;
+}
